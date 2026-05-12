2f30622429f2f096a32d278e6495ef7a741c1307
Mike <madamovych@gmail.com>
2026-05-12 21:57:34 +0300

    Cosmetic. BeanDefinition#String

--- ioc/BeanDefinition.go
+++ ioc/BeanDefinition.go
@@ -277,7 +277,7 @@ func (this *BeanDefinitionImpl[T]) getMutex() *sync.Mutex {
 
 // Implements String
 func (this *BeanDefinitionImpl[T]) String() string {
-	return fmt.Sprintf("%s[%s%s%s%s%s]", this.t,
+	return fmt.Sprintf("%s [%s%s%s%s%s]", this.t,
 		lang.If(this.scope == Singleton, "singleton", "prototype"),
 		lang.If(this.primary, " primary", ""),
 		lang.If(this.isLifecycleBean(), " Lifecycle", ""),

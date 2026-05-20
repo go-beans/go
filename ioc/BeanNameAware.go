6e53f7a646ebd1bc66334f024a98430acf7169c6
Mike <madamovych@gmail.com>
2026-05-21 01:47:14 +0300

    Cosmetic. Removed duplicated docs

--- ioc/BeanNameAware.go
+++ ioc/BeanNameAware.go
@@ -1,10 +1,5 @@
 package ioc
 
-// 1. Bean instantiation (finished)
-//
-// 2. Dependency injection (finished)
-//
-// 3. Aware callbacks (in-progress)
 type BeanNameAware interface {
 	SetBeanName(string)
 }
--- ioc/InitializingBean.go
+++ ioc/InitializingBean.go
@@ -1,14 +1,5 @@
 package ioc
 
-// 1. Bean instantiation (finished)
-//
-// 2. Dependency injection (finished)
-//
-// 3. Aware callbacks (finished)
-//
-// 4. PostConstruct (finished)
-//
-// 5. InitializingBean.AfterPropertiesSet() (in-progress)
 type InitializingBean interface {
 	AfterPropertiesSet()
 }
